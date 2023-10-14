d7dc3ebc90838ddb5d263f1911ce7940b5823ec5
Asterrix <nezirevic.enis@outlook.com>
2023-10-15 01:34:31 +0200

    3: Configure empty anchor tags in the footer component to link to new pages

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {AnchorImageComponent} from "../anchor-image/anchor-image.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: "app-footer",
   standalone: true,
-  imports: [CommonModule, AnchorImageComponent],
+  imports: [CommonModule, AnchorImageComponent, RouterLink],
   template: `
     <div class="footer">
       <div class="container">
@@ -14,17 +15,17 @@ import {AnchorImageComponent} from "../anchor-image/anchor-image.component";
           <div class="footer__section">
             <h4 class="footer__section__title">Auction</h4>
             <ul class="footer__section__list">
-              <li><a>About Us</a></li>
-              <li><a>Terms and Conditions</a></li>
-              <li><a>Privacy and policy</a></li>
+              <li><a routerLink="home/about-us" class="footer__section__item">About Us</a></li>
+              <li><a routerLink="home/terms-and-conditions" class="footer__section__item">Terms and Conditions</a></li>
+              <li><a routerLink="home/privacy-policy" class="footer__section__item">Privacy and policy</a></li>
             </ul>
           </div>
 
           <div class="footer__section">
             <h4 class="footer__section__title">Get in touch</h4>
             <ul class="footer__section__list">
-              <li><p>Call Us at &nbsp;&nbsp;+123 797-567-2535</p></li>
-              <li><a>support@auction.com</a></li>
+              <li><p class="footer__section__item footer__section__item--disabled">Call Us at &nbsp;&nbsp;+123 797-567-2535</p></li>
+              <li><a class="footer__section__item footer__section__item--disabled">support@auction.com</a></li>
               <ul class="footer__section__list footer__follow-us">
                 <li>
                   <app-anchor-image
