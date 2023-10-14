8253f021889a9f827f6da4fcfce65a8d34351be4
Asterrix <nezirevic.enis@outlook.com>
2023-10-14 23:40:07 +0200

    3: Create about us static page

---
+++ frontend/src/app/features/home/home-about-us/home-about-us.component.ts
@@ -0,0 +1,57 @@
+import {Component} from "@angular/core";
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: "app-home-about-us",
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="about-us">
+      <div class="container">
+        <div class="about-us__layout">
+          <h5>About Us</h5>
+          <div class="about-us__section">
+            <section class="about-us__section-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat pretium turpis, in
+                eleifend mi laoreet sed. Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu
+                magna. In convallis diam volutpat libero tincidunt semper. Ut aliquet erat rutrum,
+                venenatis
+                lacus ut, ornare lectus. Quisque congue ex sit amet diam malesuada, eget laoreet quam
+                molestie. In id elementum turpis. Curabitur quis tincidunt mauris.</p>
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat pretium turpis, in
+                eleifend mi laoreet sed. Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu
+                magna. In convallis diam volutpat libero tincidunt semper. Ut aliquet erat rutrum,
+                venenatis
+                lacus ut, ornare lectus. Quisque congue ex sit amet diam malesuada, eget laoreet quam
+                molestie. In id elementum turpis. Curabitur quis tincidunt mauris.</p>
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat pretium turpis, in
+                eleifend mi laoreet sed. Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu
+                magna. In convallis diam volutpat libero tincidunt semper. Ut aliquet erat rutrum,
+                venenatis
+                lacus ut, ornare lectus. Quisque congue ex sit amet diam malesuada, eget laoreet quam
+                molestie. In id elementum turpis. Curabitur quis tincidunt mauris.</p>
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat pretium turpis, in
+                eleifend mi laoreet sed. Donec ipsum mauris, venenatis sit amet porttitor id, laoreet eu
+                magna. In convallis diam volutpat libero tincidunt semper. Ut aliquet erat rutrum,
+                venenatis
+                lacus ut, ornare lectus. Quisque congue ex sit amet diam malesuada, eget laoreet quam
+                molestie. In id elementum turpis. Curabitur quis tincidunt mauris.</p>
+            </section>
+            <div class="about-us__image__section">
+              <div class="about-us__image-item"></div>
+              <div class="about-us__image-item"></div>
+              <div class="about-us__image-item"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  styleUrls: ["./home-about-us.component.scss"]
+})
+export class HomeAboutUsComponent {
+
+}
