75ac7229fb4b8bee502f3b59347d469cb405eeb2
Asterrix <nezirevic.enis@outlook.com>
2023-10-16 17:14:19 +0200

    3: Clicking on the white navbar logo routes to the home page.

--- frontend/src/app/shared/components/anchor-image/anchor-image.component.ts
+++ frontend/src/app/shared/components/anchor-image/anchor-image.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input} from "@angular/core";
 import {CommonModule, NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: "app-anchor-image",
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage, RouterLink],
   templateUrl: './anchor-image.component.html',
   styleUrls: ["./anchor-image.component.scss"]
 })
@@ -14,4 +15,5 @@ export class AnchorImageComponent {
   @Input({required: true}) alt!: string;
   @Input() imageWidth: number = 24;
   @Input() imageHeight: number = 24;
+  @Input({required: true}) externalLink!: boolean;
 }
--- frontend/src/app/shared/components/navbar/components/main-navbar/main-navbar.component.ts
+++ frontend/src/app/shared/components/navbar/components/main-navbar/main-navbar.component.ts
@@ -2,11 +2,12 @@ import {Component} from "@angular/core";
 import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {SearchBarComponent} from "../search-bar/search-bar.component";
 import {RouterLink} from "@angular/router";
+import {AnchorImageComponent} from "../../../anchor-image/anchor-image.component";
 
 @Component({
   selector: "app-main-navbar",
   standalone: true,
-  imports: [CommonModule, SearchBarComponent, NgOptimizedImage, RouterLink],
+  imports: [CommonModule, SearchBarComponent, NgOptimizedImage, RouterLink, AnchorImageComponent],
   templateUrl: "main-navbar.component.html",
   styleUrls: ["./main-navbar.component.scss"]
 })
