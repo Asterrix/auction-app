ec961dd9a5be458c42b6652a00bcf8fe0a2ce825
Asterrix <nezirevic.enis@outlook.com>
2023-10-16 09:49:51 +0200

    3: Remove margin from white navbar

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from "@angular/core";
+import {CommonModule} from "@angular/common";
 import {TopBarComponent} from "./components/top-bar/top-bar.component";
 import {MainNavbarComponent} from "./components/main-navbar/main-navbar.component";
 
 @Component({
-  selector: 'app-navbar',
+  selector: "app-navbar",
   standalone: true,
   imports: [CommonModule, TopBarComponent, MainNavbarComponent],
   template: `
-      <div class="navbar">
-          <app-top-bar/>
-          <app-main-navbar/>
-      </div>
+    <div class="navbar">
+      <app-top-bar/>
+      <app-main-navbar/>
+    </div>
   `,
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent {
 
