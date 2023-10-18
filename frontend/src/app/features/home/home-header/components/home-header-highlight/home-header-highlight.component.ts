5aafc1a795434f642820504610db0bfa813e3808
Asterrix <nezirevic.enis@outlook.com>
2023-10-18 18:16:58 +0200

    4: Create highlighted product section

---
+++ frontend/src/app/features/home/home-header/components/home-header-highlight/home-header-highlight.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: "app-home-header-highlight",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./home-header-highlight.component.html",
+  styleUrls: ["./home-header-highlight.component.scss"]
+})
+export class HomeHeaderHighlightComponent {
+
+}
