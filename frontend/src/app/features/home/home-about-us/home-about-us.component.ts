0559ba4f2121588075de4508cd38e62eb19e3439
Asterrix <nezirevic.enis@outlook.com>
2023-10-18 14:25:53 +0200

    3: Refactor home pages by making content card component reusable

--- frontend/src/app/features/home/home-about-us/home-about-us.component.ts
+++ frontend/src/app/features/home/home-about-us/home-about-us.component.ts
@@ -1,11 +1,12 @@
 import {Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {NavigationTrailComponent} from "../../../shared/components/navigation-trail/navigation-trail.component";
+import {ContentCardComponent} from "../components/content-card/content-card.component";
 
 @Component({
   selector: "app-home-about-us",
   standalone: true,
-  imports: [CommonModule, NavigationTrailComponent],
+  imports: [CommonModule, NavigationTrailComponent, ContentCardComponent],
   templateUrl: "home-about-us.component.html",
   styleUrls: ["./home-about-us.component.scss"]
 })
