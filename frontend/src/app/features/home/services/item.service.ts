b594bf7032eb6cb4bfac245d2fcfcd901c8c36c0
Asterrix <nezirevic.enis@outlook.com>
2023-10-26 15:58:24 +0200

    4: Refactor ApiService

--- frontend/src/app/features/home/services/item.service.ts
+++ frontend/src/app/features/home/services/item.service.ts
@@ -1,9 +1,5 @@
 import {Injectable} from "@angular/core";
-import {ApiService} from "../../../shared/services/api.service";
-import {Observable} from "rxjs";
 import {Page} from "../../../shared/models/interfaces/page";
-import {environment} from "../../../../environments/environment";
-import {ApiRoute} from "../../../../environments/api-route";
 import {ObservableDataManager} from "../../../shared/models/observable-data-manager";
 
 export interface FeaturedItem {
@@ -32,12 +28,12 @@ export interface ItemSummary {
   ];
 }
 
-enum SortItemsAttribute {
+export enum SortItemAttribute {
   StartDate = "startDate",
   EndDate = "endDate"
 }
 
-enum SortItemDirection {
+export enum SortItemDirection {
   ASC = "ASC",
   DESC = "DESC"
 }
@@ -48,35 +44,4 @@ enum SortItemDirection {
 export class ItemService {
   item$: ObservableDataManager<FeaturedItem> = new ObservableDataManager<FeaturedItem>();
   items$: ObservableDataManager<Page<ItemSummary>> = new ObservableDataManager<Page<ItemSummary>>();
-
-  constructor(private apiService: ApiService) {
-  }
-
-  getListOfNewestItems(): Observable<Page<ItemSummary>> {
-    return this.getListOfItems(SortItemsAttribute.StartDate, SortItemDirection.DESC);
-  }
-
-  getListOfLastChanceItems(): Observable<Page<ItemSummary>> {
-    return this.getListOfItems(SortItemsAttribute.EndDate, SortItemDirection.DESC);
-  }
-
-  getFeaturedItem(): Observable<FeaturedItem> {
-    return this.apiService.get<FeaturedItem>({
-      path: `${environment.apiUrl}/${ApiRoute.ItemRoute.Items}/${ApiRoute.ItemRoute.Featured}`,
-      options: {}
-    });
-  }
-
-  private getListOfItems(sortAttribute: string, sortDirection: string): Observable<Page<ItemSummary>> {
-    const getListOfItemsParams = {
-      page: 0,
-      size: 8,
-      sort: sortAttribute, sortDirection
-    };
-
-    return this.apiService.get<Page<ItemSummary>>({
-      path: `${environment.apiUrl}/${ApiRoute.ItemRoute.Items}`,
-      options: {params: getListOfItemsParams}
-    });
-  }
 }
