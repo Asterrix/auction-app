3801125a95fc3cd4c0213a1f25ee167ac802ac2d
Asterrix <nezirevic.enis@outlook.com>
2023-10-26 15:17:04 +0200

    4: Remove page size from environment.ts

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: "http://localhost:8080/api/v1",
-  applicationName: "Auction App",
-  pageSize: 8
+  applicationName: "Auction App"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,5 +5,4 @@ export const environment = {
   production: true,
   apiUrl: "http://localhost:8080/api/v1", // Development url
   applicationName: "Auction App",
-  pageSize: 8
 };
