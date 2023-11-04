7c995223b73c647dc2b05c99919a5df864dcc772
Asterrix <nezirevic.enis@outlook.com>
2023-11-04 22:45:42 +0100

    6: Add loaders to home page sections

--- frontend/src/app/features/home/components/home-items/home-items.component.ts
+++ frontend/src/app/features/home/components/home-items/home-items.component.ts
@@ -3,22 +3,24 @@ import {Component, Input} from "@angular/core";
 import {Router} from "@angular/router";
 import {Observable} from "rxjs";
 import {ItemCardComponent} from "../../../../shared/components/item-card/item-card.component";
+import {LoaderComponent} from "../../../../shared/components/loader/loader.component";
 import {Page} from "../../../../shared/models/interfaces/page";
 import {Api} from "../../../../shared/services/api.service";
+import {LoaderService} from "../../../../shared/services/loader.service";
 import {SectionTabComponent} from "./components/section-tab/section-tab.component";
 import ItemSummary = Api.ItemApi.Interfaces.ItemSummary;
 
 @Component({
   selector: "home-items",
   standalone: true,
-  imports: [CommonModule, SectionTabComponent, ItemCardComponent],
+  imports: [CommonModule, SectionTabComponent, ItemCardComponent, LoaderComponent],
   templateUrl: "./home-items.component.html",
   styleUrls: ["./home-items.component.scss"]
 })
 export class HomeItemsComponent {
   @Input({required: true}) items$: Observable<Page<ItemSummary> | undefined> | undefined;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public loader: LoaderService) {
   }
 
   navigateToItem(itemId: number): void {
