f583294a1adb4a0cb434888f5570f25049c73f70
Asterrix <nezirevic.enis@outlook.com>
2023-11-04 22:41:20 +0100

    6: Consolidate complex logic from dumb home components into the smart component of the home page

--- frontend/src/app/features/home/components/home-items/components/section-tab/section-tab.component.ts
+++ frontend/src/app/features/home/components/home-items/components/section-tab/section-tab.component.ts
@@ -1,16 +1,10 @@
 import {CommonModule} from "@angular/common";
-import {Component, OnDestroy, OnInit} from "@angular/core";
-import {ActivatedRoute, Params, RouterLink, RouterLinkActive} from "@angular/router";
-import {Subscription} from "rxjs";
+import {Component} from "@angular/core";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {Observable} from "rxjs";
+import {ItemTabSection, SectionTabService} from "./section-tab.service";
 
-export enum Section {
-  Default = "new-arrivals",
-  LastChance = "last-chance"
-}
 
-export enum SectionQueryParam {
-  Section = "section"
-}
 
 @Component({
   selector: "home-items-section-tab",
@@ -19,54 +13,10 @@ export enum SectionQueryParam {
   templateUrl: "./section-tab.component.html",
   styleUrls: ["./section-tab.component.scss"]
 })
-export class SectionTabComponent implements OnInit, OnDestroy {
-  sections: ({ name: string, value: Section })[] = [
-    {name: "New Arrivals", value: Section.Default},
-    {name: "Last Chance", value: Section.LastChance}
-  ];
-  activeSection: Section = Section.Default;
-  private sectionSet: Set<string> = new Set<string>(this.sections.map(section => section.value));
-  private queryParamSub: Subscription | undefined;
+export class SectionTabComponent {
+  section: Array<ItemTabSection> = this.sections.getSectionList();
+  activeSection: Observable<string> = this.sections.getActiveSection();
 
-  constructor(private activatedRoute: ActivatedRoute) {
-  }
-
-  ngOnInit(): void {
-    this.subscribeToQueryParamChanges();
-  }
-
-  ngOnDestroy(): void {
-    this.queryParamsUnsubscribe();
-  }
-
-  private queryParamsUnsubscribe(): void {
-    this.queryParamSub?.unsubscribe();
-  }
-
-  private subscribeToQueryParamChanges(): void {
-    this.queryParamSub = this.activatedRoute.queryParams.subscribe((params: Params): void => {
-      const section: string = params[SectionQueryParam.Section];
-      this.handleSectionChange(section, params);
-    });
-  }
-
-  private handleSectionChange(section: string, params: Params): void {
-    if (this.sectionExists(section)) {
-      this.setActiveSection(params);
-    } else {
-      this.setActiveSectionToDefault();
-    }
-  }
-
-  private setActiveSectionToDefault(): void {
-    this.activeSection = Section.Default;
-  }
-
-  private setActiveSection(value: Params): void {
-    this.activeSection = value[SectionQueryParam.Section];
-  }
-
-  private sectionExists(section: string): boolean {
-    return this.sectionSet.has(section);
+  constructor(private sections: SectionTabService) {
   }
 }
