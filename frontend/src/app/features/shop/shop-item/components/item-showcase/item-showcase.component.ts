dd60feb87f01c51b7ed3a8e97c87c5f242353645
Asterrix <nezirevic.enis@outlook.com>
2023-11-06 13:01:11 +0100

    6: Move complex logic from child to parent component (shop-item page)

--- frontend/src/app/features/shop/shop-item/components/item-showcase/item-showcase.component.ts
+++ frontend/src/app/features/shop/shop-item/components/item-showcase/item-showcase.component.ts
@@ -1,11 +1,9 @@
 import {CommonModule} from "@angular/common";
-import {Component, OnDestroy, OnInit} from "@angular/core";
-import {Observable, Subscription} from "rxjs";
+import {Component, Input} from "@angular/core";
+import {Observable} from "rxjs";
 import {Api} from "../../../../../shared/services/api.service";
-import {ItemService} from "../../../../../shared/services/item.service";
-import Item = Api.ItemApi.Interfaces.Item;
-import ItemImage = Api.ItemApi.Interfaces.ItemImage;
 import ItemAggregate = Api.ItemApi.Interfaces.ItemAggregate;
+import ItemImage = Api.ItemApi.Interfaces.ItemImage;
 
 @Component({
   selector: "shop-item-showcase",
@@ -14,30 +12,11 @@ import ItemAggregate = Api.ItemApi.Interfaces.ItemAggregate;
   templateUrl: "./item-showcase.component.html",
   styleUrls: ["./item-showcase.component.scss"]
 })
-export class ItemShowcaseComponent implements OnInit, OnDestroy {
-  item$: Observable<ItemAggregate | undefined> | undefined;
-  activeImage: ItemImage | undefined;
-  imagesSub: Subscription | undefined;
+export class ItemShowcaseComponent {
+  @Input({required: true}) item$: Observable<ItemAggregate | undefined> | undefined;
+  @Input({required: true}) activeImage: ItemImage | undefined;
 
-  constructor(private itemService: ItemService) {
-  }
-
-  ngOnInit(): void {
-    this.item$ = this.itemService.getItem();
-    this.imagesSub = this.item$.subscribe((item: ItemAggregate | undefined): void => {
-      if (item?.item.images && item?.item.images.length > 0) {
-        this.activeImage = item?.item.images[0];
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.imagesSub) {
-      this.imagesSub.unsubscribe();
-    }
-  }
-
-  changeActiveImage(image: ItemImage) {
+  changeActiveImage(image: ItemImage): void {
     this.activeImage = image;
   }
 }
