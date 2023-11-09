186957fcbbd94fba6257186629e3beef03ea9d62
Asterrix <nezirevic.enis@outlook.com>
2023-11-09 21:35:42 +0100

    6: Add link to item card

--- frontend/src/app/shared/components/item-card/item-card.component.ts
+++ frontend/src/app/shared/components/item-card/item-card.component.ts
@@ -1,12 +1,13 @@
 import {CommonModule} from "@angular/common";
 import {Component, Input} from "@angular/core";
+import {RouterLink} from "@angular/router";
 import {Api} from "../../services/api.service";
 import ItemSummary = Api.ItemApi.Interfaces.ItemSummary;
 
 @Component({
   selector: "app-item-card",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: "./item-card.component.html",
   styleUrls: ["./item-card.component.scss"]
 })
