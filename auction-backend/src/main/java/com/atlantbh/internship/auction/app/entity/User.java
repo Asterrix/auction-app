3838f896f84b6231e3c445f5df615593961dfbe9
Asterrix <nezirevic.enis@outlook.com>
2023-11-16 09:45:25 +0100

    8: Expand user entity attributes

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/entity/User.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/entity/User.java
@@ -2,6 +2,8 @@ package com.atlantbh.internship.auction.app.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -23,9 +25,15 @@ public class User {
     @Column(name = "password", nullable = false, length = 100)
     private String password;
 
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
     @ManyToOne(optional = false)
     @JoinColumn(name = "role_id", nullable = false)
     private Role role;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = false;
 
     public User() {
     }
@@ -34,12 +42,16 @@ public class User {
                 final String lastName,
                 final String email,
                 final String password,
-                final Role role) {
+                final LocalDate dateOfBirth,
+                final Role role,
+                final Boolean isActive) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
+        this.dateOfBirth = dateOfBirth;
         this.role = role;
+        this.isActive = isActive;
     }
 
     public Integer getId() {
@@ -78,12 +90,28 @@ public class User {
         this.password = password;
     }
 
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(final LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
     public Role getRole() {
         return role;
     }
 
     public void setRole(final Role role) {
         this.role = role;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(final Boolean active) {
+        isActive = active;
     }
 
 }
