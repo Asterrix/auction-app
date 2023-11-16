c98310d916e11181f53754520748aba2ba5e643d
Asterrix <nezirevic.enis@outlook.com>
2023-11-16 01:04:29 +0100

    8: Configure Password Encoder and User Details Service

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/config/SecurityConfig.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/config/SecurityConfig.java
@@ -5,8 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -17,7 +23,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults());
         return http.build();
     }
@@ -25,5 +31,15 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         return new CorsConfigurer(appProperties).initAllCorsConfigurations();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(final DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2B);
     }
 }
