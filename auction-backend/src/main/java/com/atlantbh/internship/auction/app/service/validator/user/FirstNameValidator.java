abd0985300c82fb1705477eab4574db2e0a8c7af
Asterrix <nezirevic.enis@outlook.com>
2023-11-16 17:21:25 +0100

    8: Implement validators for users first and last name

---
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/service/validator/user/FirstNameValidator.java
@@ -0,0 +1,17 @@
+package com.atlantbh.internship.auction.app.service.validator.user;
+
+import com.atlantbh.internship.auction.app.service.validator.Validator;
+
+public class FirstNameValidator extends Validator {
+    private static final Integer MIN_FIRST_NAME_LENGTH = 3;
+    private static final Integer MAX_FIRST_NAME_LENGTH = 20;
+
+    public static void validateFirstName(final String input) {
+        final String firstName = normaliseInput(input);
+
+        isEmptyOrNull(firstName, "First name field cannot be empty.");
+        minimumLength(firstName, MIN_FIRST_NAME_LENGTH, String.format("First name cannot contain less than %d characters", MIN_FIRST_NAME_LENGTH));
+        maximumLength(firstName, MAX_FIRST_NAME_LENGTH, String.format("First name cannot contain more than %d characters", MAX_FIRST_NAME_LENGTH));
+        isMadeUpOfLetters(firstName, "The first name must consist exclusively of letters.");
+    }
+}
