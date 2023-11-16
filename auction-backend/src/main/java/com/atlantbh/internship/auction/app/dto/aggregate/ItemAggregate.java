740fcbff331053efec690ce6d657aebc4c30e2cd
Asterrix <nezirevic.enis@outlook.com>
2023-11-16 01:00:57 +0100

    8: Move User dtos to their own package

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/dto/aggregate/ItemAggregate.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/dto/aggregate/ItemAggregate.java
@@ -1,6 +1,6 @@
 package com.atlantbh.internship.auction.app.dto.aggregate;
 
-import com.atlantbh.internship.auction.app.dto.UserItemBidDto;
+import com.atlantbh.internship.auction.app.dto.user.UserItemBidDto;
 import com.atlantbh.internship.auction.app.dto.item.ItemDto;
 import com.atlantbh.internship.auction.app.entity.Item;
 
