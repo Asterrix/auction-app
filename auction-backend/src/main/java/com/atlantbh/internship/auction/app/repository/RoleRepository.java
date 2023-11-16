d019444968dc391e24c1247e62c3de263bbc8804
Asterrix <nezirevic.enis@outlook.com>
2023-11-16 09:58:14 +0100

    8: Implement the ability to register user

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/repository/RoleRepository.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/repository/RoleRepository.java
@@ -3,5 +3,8 @@ package com.atlantbh.internship.auction.app.repository;
 import com.atlantbh.internship.auction.app.entity.Role;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends CrudRepository<Role, Integer> {
+    Optional<Role> findByRoleAllIgnoreCase(String role);
 }
