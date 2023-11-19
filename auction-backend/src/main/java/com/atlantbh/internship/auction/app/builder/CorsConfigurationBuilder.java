fff82d1c606e8907b743e2274bb865c799e47056
Asterrix <nezirevic.enis@outlook.com>
2023-11-19 13:54:08 +0100

    8: Create Cors Configuration Builder

---
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/builder/CorsConfigurationBuilder.java
@@ -0,0 +1,38 @@
+package com.atlantbh.internship.auction.app.builder;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+public class CorsConfigurationBuilder {
+    private final CorsConfiguration configuration = new CorsConfiguration();
+
+    public CorsConfigurationBuilder setAllowedOrigins(final List<String> origins) {
+        configuration.setAllowedOrigins(origins);
+        return this;
+    }
+
+    public CorsConfigurationBuilder setAllowedMethods(final String... allowedMethods) {
+        configuration.setAllowedMethods(List.of(allowedMethods));
+        return this;
+    }
+
+    public CorsConfigurationBuilder allowCredentials() {
+        configuration.setAllowCredentials(true);
+        return this;
+    }
+
+    public CorsConfiguration build() {
+        return configuration;
+    }
+
+    public static class Methods {
+        public static final String GET = HttpMethod.GET.name();
+        public static final String POST = HttpMethod.POST.name();
+        public static final String PATCH = HttpMethod.PATCH.name();
+        public static final String PUT = HttpMethod.PUT.name();
+        public static final String DELETE = HttpMethod.DELETE.name();
+    }
+}
+
--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/config/CorsConfigurer.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/config/CorsConfigurer.java
@@ -1,13 +1,15 @@
 package com.atlantbh.internship.auction.app.config;
 
+import com.atlantbh.internship.auction.app.builder.CorsConfigurationBuilder;
 import com.atlantbh.internship.auction.app.config.constant.AuctionAppProperties;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
-import java.util.List;
+
+import static com.atlantbh.internship.auction.app.builder.CorsConfigurationBuilder.Methods.GET;
+import static com.atlantbh.internship.auction.app.builder.CorsConfigurationBuilder.Methods.POST;
 
 public class CorsConfigurer {
     private final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -26,29 +28,48 @@ public class CorsConfigurer {
         return source;
     }
 
-    // By default, if the list of allowedMethods is empty, all HTTP methods will be allowed!
-    private void buildCorsConfiguration(final String path, final List<String> allowedMethods) {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList(appProperties.getClientRoute()));
-        configuration.setAllowedMethods(allowedMethods);
+    private void registerCorsConfig(final String path, final CorsConfiguration configuration) {
         source.registerCorsConfiguration(appProperties.getApiVersion() + path, configuration);
     }
 
+    // By default, if the list of allowedMethods is empty, all HTTP methods will be allowed!
     private void buildItemCorsConfiguration() {
-        buildCorsConfiguration("/items", List.of(HttpMethod.GET.name()));
-        buildCorsConfiguration("/items/{id}", List.of(HttpMethod.GET.name()));
-        buildCorsConfiguration("/items/featured", List.of(HttpMethod.GET.name()));
+        final CorsConfiguration itemsConfig = new CorsConfigurationBuilder()
+                .setAllowedOrigins(Collections.singletonList(appProperties.getClientRoute()))
+                .setAllowedMethods(GET)
+                .allowCredentials()
+                .build();
+
+        registerCorsConfig("/items/**", itemsConfig);
     }
 
     private void buildCategoryCorsConfiguration() {
-        buildCorsConfiguration("/categories", List.of(HttpMethod.GET.name()));
+        final CorsConfiguration categoriesConfig = new CorsConfigurationBuilder()
+                .setAllowedOrigins(Collections.singletonList(appProperties.getClientRoute()))
+                .setAllowedMethods(GET)
+                .allowCredentials()
+                .build();
+
+        registerCorsConfig("/categories", categoriesConfig);
     }
 
     private void buildUserRegistrationCorsConfiguration() {
-        buildCorsConfiguration("/register", List.of(HttpMethod.POST.name()));
+        final CorsConfiguration registerConfig = new CorsConfigurationBuilder()
+                .setAllowedOrigins(Collections.singletonList(appProperties.getClientRoute()))
+                .setAllowedMethods(POST)
+                .allowCredentials()
+                .build();
+
+        registerCorsConfig("/register", registerConfig);
     }
 
     private void buildUserAuthenticationCorsConfiguration() {
-        buildCorsConfiguration("/authentication", List.of(HttpMethod.POST.name()));
+        final CorsConfiguration authConfig = new CorsConfigurationBuilder()
+                .setAllowedOrigins(Collections.singletonList(appProperties.getClientRoute()))
+                .setAllowedMethods(POST)
+                .allowCredentials()
+                .build();
+
+        registerCorsConfig("/authentication", authConfig);
     }
 }
