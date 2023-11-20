00b1b02f1bb362941929f6f88673d1ced601cd2b
Asterrix <nezirevic.enis@outlook.com>
2023-11-20 22:48:01 +0100

    8: Separate public and private pages - Configure Authority claim settings

---
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/config/PagePrivacyManager.java
@@ -0,0 +1,22 @@
+package com.atlantbh.internship.auction.app.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+
+public class PagePrivacyManager {
+    protected static AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry publicPages(
+            final AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth
+    ) {
+        return auth
+                .requestMatchers("api/v1/authentication").permitAll()
+                .requestMatchers("api/v1/categories").permitAll()
+                .requestMatchers("api/v1/items/**").permitAll()
+                .requestMatchers("api/v1/register").permitAll();
+    }
+
+    protected static AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry privatePages(
+            final AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth
+    ) {
+        return auth.anyRequest().authenticated();
+    }
+}
