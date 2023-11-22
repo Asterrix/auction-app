c4a04fc4e88ab1a8a244ebc889f0c8449f68c373
Asterrix <nezirevic.enis@outlook.com>
2023-11-22 23:07:20 +0100

    8: Alert - the user that his registration was completed successfully

--- frontend/src/app/shared/components/alert/alert.component.ts
+++ frontend/src/app/shared/components/alert/alert.component.ts
@@ -2,6 +2,11 @@ import {CommonModule} from "@angular/common";
 import {Component, Input} from "@angular/core";
 import {Severity} from "../../models/errorModel";
 
+export enum AlertType {
+  Warning,
+  SuccessInfo
+}
+
 @Component({
   selector: "app-alert",
   standalone: true,
@@ -10,7 +15,9 @@ import {Severity} from "../../models/errorModel";
   styleUrls: ["./alert.component.scss"]
 })
 export class AlertComponent {
-  @Input({required: true}) severity!: Severity;
+  @Input({required: false}) severity!: Severity;
   @Input({required: true}) message!: string;
+  @Input({required: true}) type!: AlertType;
   protected readonly Severity = Severity;
+  protected readonly AlertType = AlertType;
 }
--- frontend/src/app/shared/services/user/register.service.ts
+++ frontend/src/app/shared/services/user/register.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {Router} from "@angular/router";
 import {catchError} from "rxjs";
 import {Severity} from "../../models/errorModel";
+import {AlertService} from "../alert.service";
 import {Api} from "../api.service";
 import {ErrorService} from "../error.service";
 import RegisterRequest = Api.UserApi.RegisterRequest;
@@ -10,7 +11,10 @@ import RegisterRequest = Api.UserApi.RegisterRequest;
   providedIn: "root"
 })
 export class RegisterService {
-  constructor(private apiService: Api.Service, private router: Router, private errorService: ErrorService) {
+  constructor(private apiService: Api.Service,
+              private router: Router,
+              private errorService: ErrorService,
+              private alertService: AlertService) {
   }
 
   registerUser(form: Required<RegisterRequest>) {
@@ -22,6 +26,7 @@ export class RegisterService {
         })
       )
       .subscribe(() => {
+        this.alertService.setAlert("Your account has been successfully registered. You can login now.");
         this.navigateToLoginPage();
       });
   }
