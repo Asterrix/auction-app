0922029242ed159340b30733e072bbcb73c8903d
Asterrix <nezirevic.enis@outlook.com>
2023-11-24 02:12:24 +0100

    9: Create a profile page and tab component

---
+++ frontend/src/app/features/profile/profile-page.component.ts
@@ -0,0 +1,30 @@
+import {CommonModule} from "@angular/common";
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {Observable} from "rxjs";
+import {
+  NavigationTrailService
+} from "../../shared/components/navbar/components/navigation-trail/services/navigation-trail.service";
+import {TabComponent} from "./components/tab/tab.component";
+
+@Component({
+  selector: "app-profile",
+  standalone: true,
+  imports: [CommonModule, TabComponent],
+  templateUrl: "./profile-page.component.html",
+  styleUrls: ["./profile-page.component.scss"]
+})
+export class ProfilePage implements OnInit, OnDestroy {
+  displayTrail$: Observable<boolean> | undefined;
+
+  constructor(private trailService: NavigationTrailService) {
+  }
+
+  public ngOnInit(): void {
+    this.trailService.displayNavigationTrail();
+    this.displayTrail$ = this.trailService.getDisplayTrail();
+  }
+
+  public ngOnDestroy(): void {
+    this.trailService.closeNavigationTrail();
+  }
+}
