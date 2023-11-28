9e96402f2692e4fbfc797b0fae06bf71b16e1de6
Asterrix <nezirevic.enis@outlook.com>
2023-11-28 19:31:13 +0100

    10: Fix time left calculation and formatting - Remove unique constraint on bid amount

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/mapper/UserItemBidMapper.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/mapper/UserItemBidMapper.java
@@ -9,10 +9,10 @@ public final class UserItemBidMapper {
     private UserItemBidMapper(){}
 
     public static UserItemBidDto convertToDto(final UserItemBid itemBid, final Long totalNumberOfBids){
-        return new UserItemBidDto(itemBid.getAmount(), totalNumberOfBids);
+        return new UserItemBidDto(itemBid.getAmount().toString(), totalNumberOfBids);
     }
 
     public static UserItemBidDto convertToValuesOfZeroDto(){
-        return new UserItemBidDto(new BigDecimal("0"), 0L);
+        return new UserItemBidDto("0", 0L);
     }
 }
