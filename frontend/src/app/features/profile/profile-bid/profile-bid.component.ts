04c1989dbdd09202421def9dd04b7614159f247a
Asterrix <nezirevic.enis@outlook.com>
2023-11-29 14:32:10 +0100

    10: Visual changes to bids tab

--- frontend/src/app/features/profile/profile-bid/profile-bid.component.ts
+++ frontend/src/app/features/profile/profile-bid/profile-bid.component.ts
@@ -2,8 +2,10 @@ import {CommonModule} from "@angular/common";
 import {Component, OnInit} from "@angular/core";
 import {RouterLink} from "@angular/router";
 import {Observable} from "rxjs";
+import {LoaderComponent} from "../../../shared/components/loader/loader.component";
 import {Api} from "../../../shared/services/api.service";
 import {BidService} from "../../../shared/services/bid.service";
+import {LoaderService} from "../../../shared/services/loader.service";
 import {TableDataRow} from "../components/table-data-row/table-data-row.component";
 import {TableEmptyComponent} from "../components/table-empty/table-empty.component";
 import {TableComponent} from "../components/table/table.component";
@@ -12,13 +14,14 @@ import UserBiddingInfo = Api.BidApi.UserBiddingInfo;
 @Component({
   selector: "profile-bid",
   standalone: true,
-  imports: [CommonModule, TableComponent, TableEmptyComponent, TableDataRow, RouterLink],
-  templateUrl: "./profile-bid.component.html"
+  imports: [CommonModule, TableComponent, TableEmptyComponent, TableDataRow, RouterLink, LoaderComponent],
+  templateUrl: "./profile-bid.component.html",
+  styleUrls: ["./profile-bid.component.scss"]
 })
 export class ProfileBidComponent implements OnInit {
   bids: Observable<Array<UserBiddingInfo>> | undefined;
 
-  constructor(protected biddingService: BidService) {
+  constructor(protected biddingService: BidService, protected loader: LoaderService) {
   }
 
   public ngOnInit(): void {
