8c9480c7f4e6d71e1b2fc9286ea7ff95d1d0bf30
Asterrix <nezirevic.enis@outlook.com>
2023-11-29 19:55:04 +0100

    10: Formatting

--- frontend/src/app/features/profile/components/table-empty/table-empty.component.ts
+++ frontend/src/app/features/profile/components/table-empty/table-empty.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {Component, Input} from "@angular/core";
 import {
   SecondaryButtonComponent
 } from "../../../../shared/components/buttons/secondary-button/secondary-button.component";
@@ -12,15 +12,9 @@ import {
   styleUrls: ["./table-empty.component.scss"]
 })
 export class TableEmptyComponent {
-  @Input({required: true})
-  iconPath!: string;
-
-  @Input({required: true})
-  iconName!: string;
-
-  @Input({required: true})
-  message!: string;
-
-  @Input({required: true})
-  btnLabel!: string;
+  @Input({required: true}) iconPath!: string;
+  @Input({required: true}) iconName!: string;
+  @Input({required: true}) message!: string;
+  @Input({required: true}) btnLabel!: string;
+  @Input({required: false}) redirectRoute!: string;
 }
