0f6a2a5897a49f9ace1293121ca7bfe5dd50abaa
Asterrix <nezirevic.enis@outlook.com>
2023-12-01 19:33:24 +0100

    11: Create a set date component

---
+++ frontend/src/app/features/profile/add-item/add-item-set-price-form/form-start-price/form-start-price.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form-start-price',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-start-price.component.html',
+  styleUrl: './form-start-price.component.scss'
+})
+export class FormStartPriceComponent {
+
+}
--- frontend/src/app/features/profile/add-item/add-item.component.ts
+++ frontend/src/app/features/profile/add-item/add-item.component.ts
@@ -1,11 +1,12 @@
 import {CommonModule} from "@angular/common";
 import {Component} from "@angular/core";
 import {AddItemBasicFormComponent} from "./add-item-basic-form/add-item-basic-form.component";
+import {AddItemSetPriceFormComponent} from "./add-item-set-price-form/add-item-set-price-form.component";
 
 @Component({
   selector: "profile-add-item",
   standalone: true,
-  imports: [CommonModule, AddItemBasicFormComponent],
+  imports: [CommonModule, AddItemBasicFormComponent, AddItemSetPriceFormComponent],
   templateUrl: "./add-item.component.html",
   styleUrl: "./add-item.component.scss"
 })
