d0b74b99d67b3074dd85c908549a4adc3cde97b6
Asterrix <nezirevic.enis@outlook.com>
2023-12-03 17:00:08 +0100

    11: Correct location form placeholders

--- frontend/src/app/features/profile/add-item/add-item-credit-card-form/add-item-credit-card-form.component.ts
+++ frontend/src/app/features/profile/add-item/add-item-credit-card-form/add-item-credit-card-form.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from "@angular/common";
+import {Component, Input} from "@angular/core";
+import {FormGroup} from "@angular/forms";
 import {DropdownComponent} from "../shared/dropdown/dropdown.component";
 import {FormMemberComponent} from "../shared/form-member/form-member.component";
 
 @Component({
-  selector: 'app-add-item-credit-card-form',
+  selector: "app-add-item-credit-card-form",
   standalone: true,
   imports: [CommonModule, NgOptimizedImage, FormMemberComponent, DropdownComponent],
-  templateUrl: './add-item-credit-card-form.component.html',
-  styleUrl: './add-item-credit-card-form.component.scss'
+  templateUrl: "./add-item-credit-card-form.component.html",
+  styleUrl: "./add-item-credit-card-form.component.scss"
 })
 export class AddItemCreditCardFormComponent {
-
+  @Input({required: true}) creditCardGroup!: FormGroup;
+  @Input({required: true}) credCardExpirationGroup!: FormGroup;
 }
