7e726ee8eb95dfd534af02e1a13a527dc9482bcc
Asterrix <nezirevic.enis@outlook.com>
2023-12-03 16:06:56 +0100

    11: Add initial price validation to price form

--- frontend/src/app/features/profile/add-item/add-item-set-price-form/add-item-set-price-form.component.ts
+++ frontend/src/app/features/profile/add-item/add-item-set-price-form/add-item-set-price-form.component.ts
@@ -1,16 +1,34 @@
 import {CommonModule} from "@angular/common";
-import {Component} from "@angular/core";
-import {FormsModule} from "@angular/forms";
+import {Component, inject} from "@angular/core";
+import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {FormWrapperComponent} from "../../../../shared/components/forms/form-wrapper/form-wrapper.component";
+import {
+  ValidationMessageComponent
+} from "../../../../shared/components/forms/validation-message/validation-message.component";
+import {FormFieldWrapperComponent} from "../shared/form-field-wrapper/form-field-wrapper.component";
+import {FormFieldValidator} from "../shared/validation/formFieldValidator";
 import {FormDatepicker} from "./form-date-picker/form-datepicker.component";
 import {FormStartPriceComponent} from "./form-start-price/form-start-price.component";
+import {PriceValidator} from "./validation/price-validator";
 
 @Component({
   selector: "add-item-set-price-form",
   standalone: true,
-  imports: [CommonModule, FormWrapperComponent, FormsModule, FormStartPriceComponent, FormDatepicker],
+  imports: [CommonModule, FormWrapperComponent, ReactiveFormsModule, FormsModule, FormStartPriceComponent, FormDatepicker, FormFieldWrapperComponent, ValidationMessageComponent],
   templateUrl: "./add-item-set-price-form.component.html",
   styleUrl: "./add-item-set-price-form.component.scss"
 })
 export class AddItemSetPriceFormComponent {
+  private formBuilder = inject(FormBuilder);
+  protected setPriceForm = this.formBuilder.nonNullable.group({
+    price: [""]
+  });
+  protected formControl = this.setPriceForm.controls;
+  protected validation = new FormFieldValidator([
+    {formControl: this.formControl.price, validator: new PriceValidator()}
+  ]);
+
+  public submitForm(): void {
+    this.validation.validateAllFormFields();
+  }
 }
--- frontend/src/app/features/profile/add-item/add-item-set-price-form/form-start-price/form-start-price.component.ts
+++ frontend/src/app/features/profile/add-item/add-item-set-price-form/form-start-price/form-start-price.component.ts
@@ -1,13 +1,16 @@
 import {CommonModule} from "@angular/common";
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: "app-form-start-price",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: "./form-start-price.component.html",
   styleUrl: "./form-start-price.component.scss"
 })
 export class FormStartPriceComponent {
-
+  @Input({required: true}) formFieldGroup!: FormGroup;
+  @Input({required: true}) formField!: string;
+  @Input({required: true}) isValid!: boolean;
 }
