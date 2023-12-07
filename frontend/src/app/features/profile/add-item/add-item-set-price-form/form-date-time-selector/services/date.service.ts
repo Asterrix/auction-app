f283810c04584d2a09263cee08bd15c27bfdc281
Asterrix <nezirevic.enis@outlook.com>
2023-12-07 01:18:01 +0100

    11: Refactor date service

---
+++ frontend/src/app/features/profile/add-item/add-item-set-price-form/form-date-time-selector/services/date.service.ts
@@ -0,0 +1,113 @@
+import {computed, Injectable, Signal, signal} from "@angular/core";
+import {
+  addDays,
+  addMonths,
+  eachDayOfInterval,
+  endOfMonth,
+  isAfter,
+  isSameDay,
+  isSameMonth,
+  isSameYear,
+  startOfMonth,
+  subMonths
+} from "date-fns";
+
+export interface DateChanger {
+  changeSelectedDate(date: Date): void;
+
+  goToNextMonth(): void;
+
+  goToPreviousMonth(): void;
+
+  firstDayOfMonthNumber(): number;
+
+  isSameDateIgnoreTime(date: Date): boolean;
+
+  setListOfMonthToSelectedDate(): void;
+}
+
+export type DateType = {
+  selected: Date;
+  dateList: Date[];
+}
+
+@Injectable()
+export class DateService implements DateChanger {
+  public readonly currentDate: Date = new Date();
+  private dateSignal = signal<DateType>({
+    selected: addDays(this.currentDate, 1),
+    dateList: eachDayOfInterval({
+      start: startOfMonth(this.currentDate),
+      end: endOfMonth(this.currentDate)
+    })
+  });
+
+  public date: Signal<DateType> = computed(() => this.dateSignal());
+
+
+  public changeSelectedDate(date: Date): void {
+    if (isAfter(date, this.currentDate)) {
+      this.dateSignal.set({
+        selected: date,
+        dateList: this.dateSignal().dateList
+      });
+    }
+  }
+
+  public firstDayOfMonthNumber(): number {
+    return this.getFirstListDate().getDay();
+  }
+
+  public goToNextMonth(): void {
+    const nextMonth: Date = addMonths(this.getFirstListDate(), 1);
+    const beginningOfNextMonth: Date = startOfMonth(nextMonth);
+    const endOfNextMonth: Date = endOfMonth(nextMonth);
+
+    this.dateSignal.set({
+      selected: this.dateSignal().selected,
+      dateList: eachDayOfInterval({
+        start: beginningOfNextMonth,
+        end: endOfNextMonth
+      })
+    });
+  }
+
+  public goToPreviousMonth(): void {
+    const previousMonth: Date = subMonths(this.getFirstListDate(), 1);
+
+    if (previousMonth >= startOfMonth(this.currentDate)) {
+      const beginningOfPreviousMonth: Date = startOfMonth(previousMonth);
+      const endOfPreviousMonth: Date = endOfMonth(previousMonth);
+
+      this.dateSignal.set({
+        selected: this.dateSignal().selected,
+        dateList: eachDayOfInterval({
+          start: beginningOfPreviousMonth,
+          end: endOfPreviousMonth
+        })
+      });
+    }
+  }
+
+  public isSameDateIgnoreTime(date: Date): boolean {
+    return (
+      isSameYear(this.dateSignal().selected, date) &&
+      isSameMonth(this.dateSignal().selected, date) &&
+      isSameDay(this.dateSignal().selected, date)
+    );
+  }
+
+  public setListOfMonthToSelectedDate(): void {
+    this.dateSignal.set({
+      selected: this.dateSignal().selected,
+      dateList: eachDayOfInterval({
+        start: startOfMonth(this.dateSignal().selected),
+        end: endOfMonth(this.dateSignal().selected)
+      })
+    });
+  }
+
+  private getFirstListDate(): Date {
+    return this.dateSignal().dateList[0];
+  }
+}
