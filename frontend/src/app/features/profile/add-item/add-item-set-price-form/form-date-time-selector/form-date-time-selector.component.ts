80e5eb6f97fd1081cb7d3b12097675dbe75ced4e
Asterrix <nezirevic.enis@outlook.com>
2023-12-07 02:41:46 +0100

    11: Display selected date in input field

--- frontend/src/app/features/profile/add-item/add-item-set-price-form/form-date-time-selector/form-date-time-selector.component.ts
+++ frontend/src/app/features/profile/add-item/add-item-set-price-form/form-date-time-selector/form-date-time-selector.component.ts
@@ -1,19 +1,21 @@
-import {CommonModule} from "@angular/common";
+import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {Component, EventEmitter, inject, Input, Output} from "@angular/core";
 import {ClickOutsideDirective} from "../../shared/directives/click-outside.directive";
 import {ModalComponent} from "../../shared/modal/modal.component";
-import {DatePickerComponent} from "./date-picker/date-picker.component";
-import {DateSelectorService} from "./services/date-selector.service";
+import {DateSelectorComponent} from "./date-selector/date-selector.component";
+import {DateTimeService} from "./services/date-time.service";
+import {DateService} from "./services/date.service";
+import {TimeService} from "./services/time.service";
 import {TimeSelector} from "./time-selector/time-selector.component";
 
 export interface DateTimeForm {
-  submitEvent: EventEmitter<void>;
+  submitEvent: EventEmitter<Date>;
 
   closeEvent: EventEmitter<void>;
 
   closeForm(): void;
 
-  submitForm(): void;
+  submitForm(date: Date): void;
 }
 
 enum DateTimeSelector {
@@ -27,10 +29,16 @@ enum DateTimeSelector {
   standalone: true,
   imports: [
     CommonModule,
-    DatePickerComponent,
+    DateSelectorComponent,
     ClickOutsideDirective,
     ModalComponent,
-    TimeSelector
+    TimeSelector,
+    NgOptimizedImage,
+  ],
+  providers: [
+    DateService,
+    TimeService,
+    DateTimeService
   ],
   templateUrl: "./form-date-time-selector.component.html",
   styleUrl: "./form-date-time-selector.component.scss"
@@ -38,25 +46,33 @@ enum DateTimeSelector {
 export class FormDateTimeSelectorComponent implements DateTimeForm {
   @Input({required: true}) fieldLabel!: string;
   @Output() closeEvent = new EventEmitter<void>();
-  @Output() submitEvent = new EventEmitter<void>();
+  @Output() submitEvent = new EventEmitter<Date>();
   protected currentForm: DateTimeSelector = DateTimeSelector.None;
-  private dateSelectorService = inject(DateSelectorService);
   protected readonly DatePickerForm = DateTimeSelector;
+  protected dateService: DateService = inject(DateService);
+  protected dateTimeService: DateTimeService = inject(DateTimeService);
 
   public closeForm(): void {
     if (this.currentForm === DateTimeSelector.TimeForm) {
       this.currentForm = DateTimeSelector.DateForm;
-      this.dateSelectorService.setListOfMonthToSelectedDate();
     } else {
+      this.currentForm = DateTimeSelector.None;
+    }
+    this.dateService.setListOfMonthsToSelectedDate();
+  }
+
+  public submitForm(date: Date): void {
+    if (this.currentForm === DateTimeSelector.DateForm) {
+      this.dateTimeService.setDate(date);
+      this.currentForm = DateTimeSelector.TimeForm;
+    } else if (this.currentForm === DateTimeSelector.TimeForm) {
+      this.dateTimeService.setHoursMinutes(date);
       this.currentForm = DateTimeSelector.None;
     }
   }
 
-  public submitForm(): void {
-    if (this.currentForm === DateTimeSelector.DateForm) {
-      this.currentForm++;
-    } else {
-      this.currentForm = DateTimeSelector.None;
-    }
+  protected openForm(): void {
+    this.dateService.setListOfMonthsToSelectedDate();
+    this.currentForm = DateTimeSelector.DateForm;
   }
 }
