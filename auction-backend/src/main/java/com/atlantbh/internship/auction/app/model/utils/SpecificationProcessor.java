a86e46f7079d8c311751f4c44123ce83acdf676c
Asterrix <nezirevic.enis@outlook.com>
2024-01-04 21:49:50 +0100

    13: Documentation word change

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/model/utils/SpecificationProcessor.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/model/utils/SpecificationProcessor.java
@@ -3,7 +3,7 @@ package com.atlantbh.internship.auction.app.model.utils;
 import org.springframework.data.jpa.domain.Specification;
 
 /**
- * @param <T> Specification type object
+ * @param <T> Entity type object
  * @param <U> Request type object,
  *           object that is sent from the client which contains all the filtration parameters
  *           used to create the specification
