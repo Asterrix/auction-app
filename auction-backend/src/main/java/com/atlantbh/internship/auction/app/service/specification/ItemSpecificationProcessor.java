5e9db06b2c22e40a2309e9cb55abbcacdafc2905
Asterrix <nezirevic.enis@outlook.com>
2024-01-04 21:47:43 +0100

    13: Add documentation - Remove unused category specification - Throw an exception when PriceOrder parameter does not match any of the enum values

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/service/specification/ItemSpecificationProcessor.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/service/specification/ItemSpecificationProcessor.java
@@ -7,12 +7,22 @@ import com.atlantbh.internship.auction.app.model.utils.SpecificationProcessor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 
+/**
+ * Specification processor for {@link Item} entity.
+ * Handles filtering and sorting of items based on {@link GetItemsRequest}.
+ */
 public final class ItemSpecificationProcessor implements SpecificationProcessor<Item, GetItemsRequest> {
     private final SpecificationBuilder<Item> specification = SpecificationBuilder.of(Item.class)
             .and(ItemSpecification.notFinished())
             .and(ItemSpecification.orderByNameAsc())
             .and(ItemSpecification.isActive());
 
+    /**
+     * Processes all filtering and sorting parameters for {@link Item}.
+     * Creates a single {@link Specification} for {@link Item} entity.
+     * @param request {@link GetItemsRequest} containing filtering and sorting parameters
+     * @return {@link Specification} for {@link Item} entity
+     */
     @Override
     public Specification<Item> process(final GetItemsRequest request) {
         processName(request);
@@ -22,20 +32,30 @@ public final class ItemSpecificationProcessor implements SpecificationProcessor<
         return specification.build();
     }
 
+    /**
+     * Processes name filtering for {@link Item}.
+     * @param request {@link GetItemsRequest} containing filtering and sorting parameters
+     */
     private void processName(final GetItemsRequest request) {
         if (StringUtils.isNotBlank(request.name())) {
             specification.and(ItemSpecification.hasName(request.name()));
         }
     }
 
+    /**
+     * Processes category filtering for {@link Item}.
+     * @param request {@link GetItemsRequest} containing filtering and sorting parameters
+     */
     private void processCategory(final GetItemsRequest request) {
         if (StringUtils.isNotBlank(request.subcategory()) && StringUtils.isNotBlank(request.category())) {
-            specification.and(ItemSpecification.hasSubcategory(request.category(), request.subcategory()));
-        } else if (StringUtils.isNotBlank(request.category())) {
-            specification.and(ItemSpecification.hasCategory(request.category()));
+            specification.and(ItemSpecification.hasCategory(request.category(), request.subcategory()));
         }
     }
 
+    /**
+     * Processes sorting for {@link Item}.
+     * @param request {@link GetItemsRequest} containing filtering and sorting parameters
+     */
     private void processOrderBy(final GetItemsRequest request) {
         if (StringUtils.isNotBlank(request.orderBy())) {
             switch (request.orderBy()) {
