c67e9ae47f81c5b096b6ea43d84f59a3fdb17da9
Asterrix <nezirevic.enis@outlook.com>
2024-01-08 16:42:13 +0100

    14: Change the search suggestion algorithm

--- auction-backend/src/test/java/com/atlantbh/internship/auction/app/service/featured/search/SearchSuggestionTest.java
+++ auction-backend/src/test/java/com/atlantbh/internship/auction/app/service/featured/search/SearchSuggestionTest.java
@@ -6,31 +6,35 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 class SearchSuggestionTest {
-    private final static SearchSuggestion searchSuggestion = new SearchSuggestion();
+    private final SearchSuggestion searchSuggestion = new SearchSuggestion();
 
     @Test
-    void testGetSearchSuggestion_WhenInputIsNotEmpty_ReturnMostSimilarStrings() {
-        final List<String> list = List.of("Apple", "Pine apple", "Orange", "Banana");
-        final String input = "Apple";
+    @DisplayName("Return Pineapple for input pn apple")
+    void testCalculateSimilarity_ReturnClosestMatch() {
+        final List<String> searchList = List.of(
+                "Apple",
+                "Pineapple",
+                "Orange",
+                "Banana",
+                "Raspberry"
+        );
+        final String inputStr = "pn apple";
 
-        final List<String> suggestions = searchSuggestion.searchSuggestion(list, input);
+        final List<String> searchSuggestions = searchSuggestion.createSearchSuggestion(searchList, inputStr);
 
-        assertEquals(4, suggestions.size());
-        assertEquals("Apple", suggestions.getFirst());
-        assertEquals("Orange", suggestions.get(1));
-        assertEquals("Banana", suggestions.get(2));
-        assertEquals("Pine apple", suggestions.getLast()); // Last because of the size difference.
+        assertEquals("Pineapple should be the closest match", searchList.get(1).toLowerCase(), searchSuggestions.getFirst().toLowerCase());
+        assertEquals("Apple should be the second closest match", searchList.get(0).toLowerCase(), searchSuggestions.get(1).toLowerCase());
     }
 
     @Test
     @DisplayName("One string is longer than another.")
     void testGetSearchSuggestion_WhenOneStringIsLongerThanAnother_DontThrowIndexOutOfBoundsException() {
-        final List<String> list = List.of("Pine apple");
+        final List<String> list = List.of("Pineapple");
         final String input = "Apple";
 
-        assertDoesNotThrow(() -> searchSuggestion.searchSuggestion(list, input), "Index is out of bounds.");
+        assertDoesNotThrow(() -> searchSuggestion.createSearchSuggestion(list, input), "Index is out of bounds.");
     }
 }
