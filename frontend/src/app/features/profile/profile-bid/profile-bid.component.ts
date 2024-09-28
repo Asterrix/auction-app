73e5fbf8ace156b21f1b6d8ff9f37652d55377f3
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:29 +0200

    9: Remove the view button in favor of a click event

--- frontend/src/app/features/profile/profile-bid/profile-bid.component.ts
+++ frontend/src/app/features/profile/profile-bid/profile-bid.component.ts
@@ -1,12 +1,14 @@
 import {CommonModule} from "@angular/common";
 import {Component} from "@angular/core";
+import {RouterLink} from "@angular/router";
+import {TableDataRow} from "../components/table-data-row/table-data-row.component";
 import {TableEmptyComponent} from "../components/table-empty/table-empty.component";
 import {TableComponent} from "../components/table/table.component";
 
 @Component({
   selector: "profile-bid",
   standalone: true,
-  imports: [CommonModule, TableComponent, TableEmptyComponent],
+  imports: [CommonModule, TableComponent, TableEmptyComponent, TableDataRow, RouterLink],
   templateUrl: "./profile-bid.component.html"
 })
 export class ProfileBidComponent {
