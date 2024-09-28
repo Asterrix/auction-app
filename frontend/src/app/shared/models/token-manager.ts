ea081c7f6895e1e98424c5167c45549ed401cdf9
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:24 +0200

    8: Implement Remember Me functionality

--- frontend/src/app/shared/models/token-manager.ts
+++ frontend/src/app/shared/models/token-manager.ts
@@ -1,4 +1,6 @@
 import {HttpResponse} from "@angular/common/http";
+import {jwtDecode} from "jwt-decode";
+import {UserDetails} from "../services/authentication.service";
 
 enum Token {
   Key = "Token",
@@ -6,19 +8,37 @@ enum Token {
 }
 
 export class TokenManager {
-  static retrieveToken(httpResponse: HttpResponse<void>): string | null {
+  static retrieveTokenFromHeader(httpResponse: HttpResponse<void>): string | null {
     return httpResponse.headers.get(Token.Header);
   }
 
-  static retrieveTokenFromStorage(): string | null {
+  static retrieveTokenFromSessionStorage(): string | null {
     return window.sessionStorage.getItem(Token.Key);
   }
 
-  static storeToken(token: string): void {
+  static retrieveTokenFromLocalStorage(): string | null {
+    return window.localStorage.getItem(Token.Key);
+  }
+
+  static storeTokenToSessionStorage(token: string): void {
     window.sessionStorage.setItem(Token.Key, JSON.stringify(token));
+  }
+
+  static storeTokenToLocalStorage(token: string): void {
+    window.localStorage.setItem(Token.Key, JSON.stringify(token));
   }
 
   static removeToken(): void {
     window.sessionStorage.removeItem(Token.Key);
+    window.localStorage.removeItem(Token.Key);
+  }
+
+  static decodeToken(sessionToken: string): UserDetails {
+    return jwtDecode(sessionToken);
+  }
+
+  static determineLocationToStoreToken(rememberMe: boolean, token: string): void {
+    if (rememberMe) TokenManager.storeTokenToLocalStorage(token);
+    else TokenManager.storeTokenToSessionStorage(token);
   }
 }
