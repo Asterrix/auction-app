629aa54e21d96d450a38c44ed934589ae061d1ef
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:15 +0200

    4: Replace getAll endpoint parameters with Pageable interface

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/repository/ItemRepository.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/repository/ItemRepository.java
@@ -2,8 +2,6 @@ package com.atlantbh.internship.auction.app.repository;
 
 import com.atlantbh.internship.auction.app.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/service/ItemService.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/service/ItemService.java
@@ -2,9 +2,8 @@ package com.atlantbh.internship.auction.app.service;
 
 import com.atlantbh.internship.auction.app.dto.ItemSummaryDto;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
 
-@Service
 public interface ItemService {
-    Page<ItemSummaryDto> getAll(final Integer pageNumber, final Integer pageSize, final String sortByAttribute, final String sortDirection);
+    Page<ItemSummaryDto> getAll(final Pageable pageable);
 }
