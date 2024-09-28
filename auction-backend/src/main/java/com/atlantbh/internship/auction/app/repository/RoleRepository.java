addb6aa8a13f767d3a00cbdb91821956be0722b8
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:22 +0200

    8: Create user and role repository

---
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.atlantbh.internship.auction.app.repository;
+
+import com.atlantbh.internship.auction.app.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+}
