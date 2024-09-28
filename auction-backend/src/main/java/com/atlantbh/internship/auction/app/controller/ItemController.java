d4f5929522f5e833070793eca144e841ac84002a
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:14 +0200

    4: Refactor CORS handling and ItemController constants

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/controller/ItemController.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/controller/ItemController.java
@@ -1,29 +1,35 @@
 package com.atlantbh.internship.auction.app.controller;
 
-import com.atlantbh.internship.auction.app.config.constant.ItemGetAllRequestConstant;
 import com.atlantbh.internship.auction.app.dto.ItemSummaryDto;
 import com.atlantbh.internship.auction.app.service.ItemService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/items")
-@CrossOrigin(origins = "http://localhost:4200/")
 public class ItemController {
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "8";
+    public static final String DEFAULT_SORT_BY_ATTRIBUTE = "id";
+    public static final String DEFAULT_SORT_DIRECTION = "ASC";
+
     private final ItemService itemService;
 
-    public ItemController(ItemService itemService) {
+    public ItemController(final ItemService itemService) {
         this.itemService = itemService;
     }
 
     @GetMapping
     public ResponseEntity<Page<ItemSummaryDto>> getAllItems(
-            @RequestParam(defaultValue = ItemGetAllRequestConstant.DEFAULT_PAGE_NUMBER) Integer pageNumber,
-            @RequestParam(defaultValue = ItemGetAllRequestConstant.DEFAULT_PAGE_SIZE) Integer pageSize,
-            @RequestParam(defaultValue = ItemGetAllRequestConstant.DEFAULT_SORT_BY_ATTRIBUTE) String sortByAttribute,
-            @RequestParam(defaultValue = ItemGetAllRequestConstant.DEFAULT_SORT_DIRECTION) String sortDirection) {
+            @RequestParam(defaultValue = DEFAULT_PAGE_NUMBER) Integer pageNumber,
+            @RequestParam(defaultValue = DEFAULT_PAGE_SIZE) Integer pageSize,
+            @RequestParam(defaultValue = DEFAULT_SORT_BY_ATTRIBUTE) String sortByAttribute,
+            @RequestParam(defaultValue = DEFAULT_SORT_DIRECTION) String sortDirection) {
 
         Page<ItemSummaryDto> response = this.itemService.getAll(pageNumber, pageSize, sortByAttribute, sortDirection);
         return new ResponseEntity<>(response, HttpStatus.OK);
