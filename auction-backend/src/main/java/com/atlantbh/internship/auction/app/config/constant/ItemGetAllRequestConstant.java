d94d5b1a589d91b2aa744ca7647d81cc6393336e
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:14 +0200

    4: Replace old ItemDto with a new Dto which displays minimum level of entity details - Add constants to getAllItems request in ItemController - Adapt the controller to use new Dto

---
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/config/constant/ItemGetAllRequestConstant.java
@@ -0,0 +1,8 @@
+package com.atlantbh.internship.auction.app.config.constant;
+
+public class ItemGetAllRequestConstant {
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "8";
+    public static final String DEFAULT_SORT_BY_ATTRIBUTE = "id";
+    public static final String DEFAULT_SORT_DIRECTION = "ASC";
+}
