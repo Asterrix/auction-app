9fa70457ff7473b1e073af03f407b3baf43cf722
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:28 +0200

    8: Refactoring

--- frontend/src/app/features/register/components/register-form/validators/last-name.validator.ts
+++ frontend/src/app/features/register/components/register-form/validators/last-name.validator.ts
@@ -1,15 +1,15 @@
 import {NameValidator} from "./name-validator";
-import {NameValidatorError} from "./name-validator-error";
+import NameValidatorError = NameValidator.NameValidatorError;
 
 export class LastNameValidator {
   protected static readonly minLength: number = 3;
   protected static readonly maxLength: number = 30;
 
   public static message: Record<NameValidatorError, string> = {
-    [NameValidatorError.REQUIRED]: `Last name field cannot be empty.`,
-    [NameValidatorError.MIN_LENGTH]: `Last name cannot contain less than ${this.minLength} characters.`,
-    [NameValidatorError.MAX_LENGTH]: `Last name cannot contain more than ${this.maxLength} characters.`,
-    [NameValidatorError.LETTERS]: `The last name must consist exclusively of letters.`,
+    [NameValidatorError.Required]: `Last name field cannot be empty.`,
+    [NameValidatorError.MinLength]: `Last name cannot contain less than ${this.minLength} characters.`,
+    [NameValidatorError.MaxLength]: `Last name cannot contain more than ${this.maxLength} characters.`,
+    [NameValidatorError.Letters]: `The last name must consist exclusively of letters.`,
   };
 
   public static config: NameValidator.Config = {
--- frontend/src/app/features/register/components/register-form/validators/name-validator.ts
+++ frontend/src/app/features/register/components/register-form/validators/name-validator.ts
@@ -7,51 +7,54 @@ import {
   minLengthFn
 } from "../../../../../shared/models/validators/validator-functions";
 import {RegisterForm} from "../register-form.component";
-import {NameValidatorError} from "./name-validator-error";
 import {ValidationPair} from "./validation.pair";
 
 export namespace NameValidator {
+  export enum NameValidatorError {
+    Required,
+    MinLength,
+    MaxLength,
+    Letters
+  }
+
   export interface Config {
     minLength: number;
     maxLength: number;
     messages: Record<NameValidatorError, string>;
   }
 
+  const validationErrors = [
+    NameValidatorError.Required,
+    NameValidatorError.MinLength,
+    NameValidatorError.MaxLength,
+    NameValidatorError.Letters,
+  ];
+
   export function validator(config: Config): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const name: string = control.value;
 
       const empty: boolean = isEmptyFn(name);
-      if (empty) return {[NameValidatorError.REQUIRED]: true};
+      if (empty) return {[NameValidatorError.Required]: true};
 
       const minimumLength: boolean = minLengthFn(name, config.minLength);
-      if (minimumLength) return {[NameValidatorError.MIN_LENGTH]: true};
+      if (minimumLength) return {[NameValidatorError.MinLength]: true};
 
       const maximumLength: boolean = maxLengthFn(name, config.maxLength);
-      if (maximumLength) return {[NameValidatorError.MAX_LENGTH]: true};
+      if (maximumLength) return {[NameValidatorError.MaxLength]: true};
 
       const containsSpecialCharacters: boolean = isMadeUpOfLettersFn(name);
-      if (containsSpecialCharacters) return {[NameValidatorError.LETTERS]: true};
+      if (containsSpecialCharacters) return {[NameValidatorError.Letters]: true};
 
-      return null;
+      return {};
     };
   }
 
   export function validateName(formGroup: FormGroup, fieldName: RegisterForm, config: Config): ValidationPair {
-    if (isFieldValidFn(formGroup, fieldName, NameValidatorError.REQUIRED.toString())) {
-      return {valid: false, message: config.messages[NameValidatorError.REQUIRED]};
-    }
-
-    if (isFieldValidFn(formGroup, fieldName, NameValidatorError.MIN_LENGTH.toString())) {
-      return {valid: false, message: config.messages[NameValidatorError.MIN_LENGTH]};
-    }
-
-    if (isFieldValidFn(formGroup, fieldName, NameValidatorError.MAX_LENGTH.toString())) {
-      return {valid: false, message: config.messages[NameValidatorError.MAX_LENGTH]};
-    }
-
-    if (isFieldValidFn(formGroup, fieldName, NameValidatorError.LETTERS.toString())) {
-      return {valid: false, message: config.messages[NameValidatorError.LETTERS]};
+    for (const errorType of validationErrors) {
+      if (isFieldValidFn(formGroup, fieldName, errorType.toString())) {
+        return {valid: false, message: config.messages[errorType]};
+      }
     }
 
     return {valid: true};
