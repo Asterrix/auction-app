361b7c3dee3682632cff1fd4e035aeb66a13b14f
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:32 +0200

    11: Create featured products component

--- frontend/src/app/features/profile/add-item/add-item-location-shipping/add-item-location-shipping.component.ts
+++ frontend/src/app/features/profile/add-item/add-item-location-shipping/add-item-location-shipping.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormWrapperComponent} from "../../../../shared/components/forms/form-wrapper/form-wrapper.component";
 import {DropdownComponent} from "../add-item-basic-form/components/form-categories/dropdown/dropdown.component";
+import {AddItemCreditCardFormComponent} from "../add-item-credit-card-form/add-item-credit-card-form.component";
 import {FormMemberComponent} from "../shared/form-member/form-member.component";
 import {FormPhoneNumberComponent} from "./form-phone-number/form-phone-number.component";
 
 @Component({
   selector: 'app-add-item-location-shipping',
   standalone: true,
-  imports: [CommonModule, FormWrapperComponent, DropdownComponent, FormMemberComponent, FormPhoneNumberComponent],
+  imports: [CommonModule, FormWrapperComponent, DropdownComponent, FormMemberComponent, FormPhoneNumberComponent, AddItemCreditCardFormComponent],
   templateUrl: './add-item-location-shipping.component.html',
   styleUrl: './add-item-location-shipping.component.scss'
 })
