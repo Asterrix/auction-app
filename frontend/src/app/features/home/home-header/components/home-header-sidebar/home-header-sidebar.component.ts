2b0d5450756004f8d3266e5d3d4363a8bb22641b
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:15 +0200

    4: Fix memory leak related to categories

--- frontend/src/app/features/home/home-header/components/home-header-sidebar/home-header-sidebar.component.ts
+++ frontend/src/app/features/home/home-header/components/home-header-sidebar/home-header-sidebar.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {Category, ItemCategoryService} from "../services/item-category.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: "app-home-header-sidebar",
@@ -9,19 +10,27 @@ import {Category, ItemCategoryService} from "../services/item-category.service";
   templateUrl: "./home-header-sidebar.component.html",
   styleUrls: ["./home-header-sidebar.component.scss"]
 })
-export class HomeHeaderSidebarComponent implements OnInit {
+export class HomeHeaderSidebarComponent implements OnInit, OnDestroy {
+  private _getListOfCategoriesSub: Subscription | undefined;
+
+  constructor(private categoryService: ItemCategoryService) {
+  }
+
   private _categories: Array<Category> = new Array<Category>();
 
   get categories(): Array<Category> {
     return this._categories;
   }
 
-  constructor(private categoryService: ItemCategoryService) {
-  }
-
   ngOnInit(): void {
-    this.categoryService.getListOfCategories().subscribe(categories => {
+    this._getListOfCategoriesSub = this.categoryService.getListOfCategories().subscribe(categories => {
       this._categories = categories;
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this._getListOfCategoriesSub != undefined) {
+      this._getListOfCategoriesSub.unsubscribe();
+    }
   }
 }
