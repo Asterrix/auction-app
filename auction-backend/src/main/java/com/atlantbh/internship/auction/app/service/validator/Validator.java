40267ec7aef85f94d32add1d3147418d951e950b
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:23 +0200

    8: Implement validators for users first and last name

---
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/service/validator/Validator.java
@@ -0,0 +1,41 @@
+package com.atlantbh.internship.auction.app.service.validator;
+
+import com.atlantbh.internship.auction.app.exception.ValidationException;
+
+
+public class Validator {
+    protected static void isEmptyOrNull(final String input, final String message) {
+        if (input == null || input.isEmpty()) {
+            throw new ValidationException(message);
+        }
+    }
+
+    protected static void minimumLength(final String input, final int minimumLength, final String message) {
+        if (input == null || input.length() < minimumLength) {
+            throw new ValidationException(message);
+        }
+    }
+
+    protected static void maximumLength(final String input, final int maximumLength, final String message) {
+        if (input == null || input.length() > maximumLength) {
+            throw new ValidationException(message);
+        }
+    }
+
+    protected static String normaliseInput(final String input) {
+        return input.replaceAll("\\s+", " ").trim();
+    }
+
+    protected static void isMadeUpOfLetters(final String input, final String message) {
+        if (input == null) throw new ValidationException(message);
+        final int n = input.length();
+
+        for (int i = 0; i < n; i++) {
+            if (input.charAt(i) == ' ') {
+                continue;
+            } else if (!Character.isLetter(input.charAt(i))) {
+                throw new ValidationException(message);
+            }
+        }
+    }
+}
