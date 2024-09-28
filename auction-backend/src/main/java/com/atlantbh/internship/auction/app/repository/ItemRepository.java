66495742749a3ac831dce7fcfbb02e0667ebb453
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:17 +0200

    4: Add additional logic to getFeaturedItem repository method

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/repository/ItemRepository.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/repository/ItemRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-    @Query("select f.id as id, f.name as name, f.description as description, f.initialPrice as initialPrice from Item f where f.id = :itemId")
-    Optional<ItemInfo> getFeaturedItem(@Param("itemId") Integer itemId);
+    @Query(value = "SELECT f.id as id, f.name as name, f.description as description, f.initial_price as InitialPrice " +
+            "from items as f where f.id is not null and f.end_date > ? limit 1", nativeQuery = true)
+    Optional<ItemInfo> getFeaturedItem(@Param("endDate") LocalDate endDate);
 }
