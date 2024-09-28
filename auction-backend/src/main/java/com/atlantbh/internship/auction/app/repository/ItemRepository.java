0a2b6e6ddb831881a0992586a551e2963f653802
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:17 +0200

    4: Improve featured item image query

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/repository/ItemRepository.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/repository/ItemRepository.java
@@ -15,6 +15,7 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
     @Query("select f.id as id, f.name as name, f.description as description, f.initialPrice as initialPrice from Item f where f.id = :itemId")
     Optional<ItemInfo> getFeaturedItem(@Param("itemId") Integer itemId);
 
-    @Query(value = "SELECT i.id as id, i.name as name, i.image_url as imageUrl FROM items_images i WHERE i.item_id = :itemId LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT i.id as id, i.name as name, i.image_url as imageUrl FROM items_images i WHERE i.item_id = :itemId " +
+            "order by i.id asc LIMIT 1", nativeQuery = true)
     Optional<ItemImageInfo> getFeaturedItemImage(@Param("itemId") Integer itemId);
 }
