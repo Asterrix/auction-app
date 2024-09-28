43a43dc63f6c677ca2fe0903f9471d17f91a562b
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:22 +0200

    8: Expand user properties and add Role entity

--- auction-backend/src/main/java/com/atlantbh/internship/auction/app/entity/User.java
+++ auction-backend/src/main/java/com/atlantbh/internship/auction/app/entity/User.java
@@ -11,11 +11,79 @@ public class User {
     @Column(name = "id", nullable = false)
     private Integer id;
 
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 30)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true, length = 40)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
     public User() {
+    }
+
+    public User(final String firstName,
+                final String lastName,
+                final String email,
+                final String password,
+                final Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
     }
 
     public Integer getId() {
         return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(final Role role) {
+        this.role = role;
     }
 
 }
---
+++ auction-backend/src/main/resources/db/migration/roles_sample.sql
@@ -0,0 +1,1 @@
+INSERT INTO public.roles(id, role) VALUES (1, 'Admin');
