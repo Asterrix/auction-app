12f977fb14eb8089d7f6aab9e300edddb99f7f69
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-28 22:00:32 +0200

    11: Correct location form placeholders

--- frontend/src/app/features/profile/add-item/add-item-location-shipping/add-item-location-shipping.component.ts
+++ frontend/src/app/features/profile/add-item/add-item-location-shipping/add-item-location-shipping.component.ts
@@ -1,18 +1,50 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from "@angular/common";
+import {Component, inject} from "@angular/core";
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 import {FormWrapperComponent} from "../../../../shared/components/forms/form-wrapper/form-wrapper.component";
-import {DropdownComponent} from "../shared/dropdown/dropdown.component";
+import {
+  ValidationMessageComponent
+} from "../../../../shared/components/forms/validation-message/validation-message.component";
 import {AddItemCreditCardFormComponent} from "../add-item-credit-card-form/add-item-credit-card-form.component";
+import {DropdownComponent} from "../shared/dropdown/dropdown.component";
+import {FormFieldWrapperComponent} from "../shared/form-field-wrapper/form-field-wrapper.component";
 import {FormMemberComponent} from "../shared/form-member/form-member.component";
+import {FormPairs} from "../shared/validation/formFieldValidator";
 import {FormPhoneNumberComponent} from "./form-phone-number/form-phone-number.component";
 
 @Component({
-  selector: 'app-add-item-location-shipping',
+  selector: "app-add-item-location-shipping",
   standalone: true,
-  imports: [CommonModule, FormWrapperComponent, DropdownComponent, FormMemberComponent, FormPhoneNumberComponent, AddItemCreditCardFormComponent],
-  templateUrl: './add-item-location-shipping.component.html',
-  styleUrl: './add-item-location-shipping.component.scss'
+  imports: [CommonModule, FormWrapperComponent, DropdownComponent, FormMemberComponent, FormPhoneNumberComponent, AddItemCreditCardFormComponent, FormFieldWrapperComponent, ValidationMessageComponent, ReactiveFormsModule],
+  templateUrl: "./add-item-location-shipping.component.html",
+  styleUrl: "./add-item-location-shipping.component.scss"
 })
 export class AddItemLocationShippingComponent {
+  private formBuilder = inject(FormBuilder);
+  protected locationShippingForm = this.formBuilder.nonNullable.group({
+    address: [""],
+    email: [""],
+    location: this.formBuilder.nonNullable.group({
+      city: [""],
+      zip: [""],
+      country: [""]
+    }),
+    contact: [""],
+    creditCard: this.formBuilder.nonNullable.group({
+      nameOnCard: [""],
+      cardNumber: [""],
+      expiration: this.formBuilder.nonNullable.group({
+        month: [""],
+        year: [""],
+        cvc: [""]
+      })
+    })
+  });
+  protected formControl = this.locationShippingForm.controls;
 
+  protected changeCountry(country: string) {
+    this.formControl.location.patchValue({
+      country: country
+    });
+  }
 }
--- frontend/src/app/features/profile/add-item/add-item-location-shipping/form-phone-number/form-phone-number.component.ts
+++ frontend/src/app/features/profile/add-item/add-item-location-shipping/form-phone-number/form-phone-number.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from "@angular/core";
 import { CommonModule } from '@angular/common';
+import {FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-form-phone-number',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-phone-number.component.html',
   styleUrl: './form-phone-number.component.scss'
 })
 export class FormPhoneNumberComponent {
-
+  @Input({required: true}) controlGroup!: FormGroup;
+  @Input({required: true}) controlName!: string;
 }
